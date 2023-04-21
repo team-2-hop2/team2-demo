462cf93730b3d18eacd030bd67253de005f49018
Buyantogtokh <HOP2-Buyantogtokh@Iveelts-iMac.local>
2023-04-21 16:32:03 +0800

    14-productCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,22 @@ module.exports = {
         'purple' :'#BA90C6',
         'blue': '#C0DBEA',
       },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
     },
   },
   plugins: [],
