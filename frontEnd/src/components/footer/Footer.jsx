9e41d334aaad7f7d8f753ad26e3215d91a5e7ade
Booyah <99385673+Buyantogtoh0425@users.noreply.github.com>
2023-05-23 15:10:46 +0800

    header-footer

--- frontEnd/src/components/footer/Footer.jsx
+++ frontEnd/src/components/footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import logo from './Logo dan tsagaan.png'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-blue flex flex-col items-center gap-10'>
+      <div className='lg:flex lg:justify-around w-screen lg:items-center'>
+        <div className='flex  items-center'>
+          <img alt='' src={logo} className='h-[100px]' />
+          <FacebookIcon className='text-white' />
+          <InstagramIcon className='text-white'/>
+        </div>
+        <div className='flex flex-col pl-8 gap-1'>
+          <p className='font-[Shantell] text-center text-[white]'>ХОЛБОО БАРИХ</p>
+          <p className='font-[Shantell] text-[white]'>Утас: 76099393</p>
+          <p className='font-[Shantell] text-[white]'>И-мэйл хаяг: the93kidult@gmail.com</p>
+        </div>
+      </div>
+      <p className='font-[Shantell] text-[white]'>2023 © TERSKHEN KID'S BRAND</p>
+
+    </div>
   )
 }
--- frontEnd/src/components/header/Header.jsx
+++ frontEnd/src/components/header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import logo from './Logo dan tsagaan.png'
+import cartIcon from './CartIcon.png'
+import profIcon from './profIcon.png'
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-screen bg-blue flex items-center justify-between px-3' >
+      <img alt='' src={logo} className='h-[100px]' />
+      <div className='flex gap-1'>
+        <PersonIcon className='text-white '  />
+        <ShoppingCartIcon className='text-white' />
+      </div>
+    </div>
   )
 }
